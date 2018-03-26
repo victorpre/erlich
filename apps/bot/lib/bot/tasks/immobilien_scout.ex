7229a1c4fd19031fb163ad23886cb6c953b2f459
Victor Presumido <victor.presumido@gmail.com>
2018-03-26 19:05:47 -0300

    🏁 Finish task to import/notify subscribers

--- apps/bot/lib/bot/tasks/immobilien_scout.ex
+++ apps/bot/lib/bot/tasks/immobilien_scout.ex
@@ -3,6 +3,7 @@ defmodule Bot.Tasks.ImmobilienScout do
 
   alias Crawler.ImmobilienScout
   alias Bot.Homes
+  alias Bot.Recipients
 
   def import_ads(page \\ 1) do
     Logger.info("Checking ImmobilienScout for updates...")
@@ -13,7 +14,7 @@ defmodule Bot.Tasks.ImmobilienScout do
         new_entries = process_entries(entries)
 
         if (length(new_entries) > 0) do
-          # notify_subscribers(new_entries)
+          notify_subscribers(new_entries)
           import_ads(page + 1)
         end
     end
@@ -27,8 +28,18 @@ defmodule Bot.Tasks.ImmobilienScout do
 
   defp insert_entry(entry) do
     case Homes.get_ad_by_url(entry.url) do
-      nil -> Homes.create_ad(entry)
+      nil -> Homes.create_ad!(entry)
       _other -> nil
     end
+  end
+
+  defp notify_subscribers(ads) do
+    subscribers = Recipients.list_subscribers()
+
+    Enum.each(ads, fn ad ->
+      Enum.each(subscribers, fn x ->
+        Recipients.notify_subscriber(x, ad)
+      end)
+    end)
   end
 end
