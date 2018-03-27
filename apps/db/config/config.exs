ee1f6a1145fd09d004de6c1aa53e61dc91c1062c
Victor Presumido <victor.presumido@gmail.com>
2018-03-27 01:15:30 -0300

    Fix deploy config

--- apps/db/config/config.exs
+++ apps/db/config/config.exs
@@ -2,7 +2,7 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :db,
+config :db, Db.Repo,
   ecto_repos: [Db.Repo]
 
 # This configuration is loaded before any dependency and is restricted
