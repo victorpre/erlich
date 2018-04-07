85fc66b0f5b57016311295aa963b6f78d9c91876
Victor Presumido <victor.presumido@gmail.com>
2018-04-07 11:19:31 -0300

    Finish data import from Akelius

--- apps/crawler/lib/akelius.ex
+++ apps/crawler/lib/akelius.ex
@@ -1,13 +1,11 @@
 defmodule Crawler.Akelius do
-
   def import(page \\ 1) do
     url = "https://www.akelius.de/en/search/apartments/norden/hamburg/list?region=all"
 
     url
     |> get_page_html()
     |> get_dom_elements()
-    |> filter_ads()
-    # |> extract_metadata()
+    |> extract_metadata()
   end
 
   defp get_page_html(url) do
@@ -16,8 +14,48 @@ defmodule Crawler.Akelius do
   end
 
   defp get_dom_elements(body) do
+    Floki.find(body, "li.no-border > figure")
   end
 
-  defp filter_ads(elements) do
+  defp extract_metadata(elements) do
+    Enum.map(elements, fn {"figure", attrs, content} ->
+      %{
+        title: title(content),
+        url: url(content),
+        price: price(content),
+        size: size(content),
+        img: image(content),
+        provider: "Akelius"
+      }
+    end)
+  end
+
+  defp title(html) do
+    [{"h3", _attribs, title}] = Floki.find(html, "figcaption h3")
+
+    title
+    |> Enum.take_every(2)
+    |> Enum.map(fn x -> String.trim(x) end)
+    |> Enum.join(", ")
+  end
+
+  defp url(html) do
+    [{"a", [{"href", url} | _params], _other_params}] = Floki.find(html, "a")
+    "https://www.akelius.de" <> url
+  end
+
+  defp price(html) do
+    [{_, _, [price]}] = Floki.find(html, "p span.price")
+    price
+  end
+
+  defp size(html) do
+    [{_, _, [size]}] = Floki.find(html, "p span.areaSize")
+    size
+  end
+
+  defp image(html) do
+    [{"div", [_, {"style", img_asset}], _}] = Floki.find(html, ".image-box")
+    String.slice(img_asset, 22..-2)
   end
 end
